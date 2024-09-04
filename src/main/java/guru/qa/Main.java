f00bc3c42fbda45651f3cd6144aa6bdb6fcfc8d5
Anton Zhilkin <tonyzhilkin@gmail.com>
2024-09-05 01:42:48 +0300

    Added getMessage method

--- src/main/java/guru/qa/Main.java
+++ src/main/java/guru/qa/Main.java
@@ -2,6 +2,7 @@ package guru.qa;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello QA guru!");
+        System.out.println(getMessage());
     }
+    public static String getMessage() {return "Hello from getMessage"; };
 }
